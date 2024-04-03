a05265951b2cfe460c97fc2689abb5cd6f12b914
doctor-acid <avnishbadoni@gmail.com>
2024-04-03 16:18:36 +0530

    jwt service

--- src/server/util/jwtActions.ts
+++ src/server/util/jwtActions.ts
@@ -1,20 +1,23 @@
 
 import jwt from 'jsonwebtoken'
 
-let JWT_SECRET = "my_temp_secret_derived_from_env"
+let JWT_SECRET = "my_temp_secret_derived_from_env";
+
+let TEMP_JWT_SECRET = "my_temp_secret_for_unverified_customer"
 
 type jwt_token_data = {
-    id : string
+    id : number
     type: "ADMIN" | "USER"
 
 }
 
-export function decodeToken(token: string): jwt_token_data{
-    return jwt.verify(token, JWT_SECRET) as jwt_token_data;
+export function decodeToken(token: string, temp:boolean=false): jwt_token_data{
+    let MY_JWT_SECRET = temp? TEMP_JWT_SECRET : JWT_SECRET
+    return jwt.verify(token, MY_JWT_SECRET) as jwt_token_data;
 }
 
-export function encodeToken(jwt_data : jwt_token_data): string{
+export function encodeToken(jwt_data : jwt_token_data, temp:boolean=false): string{
     let expiry = (Number(process.env.JWT_EXPIRATION_TIME_MINUTES)*60) || '24h'
-
-    return jwt.sign(jwt_data, JWT_SECRET, { expiresIn : expiry})
+    let MY_JWT_SECRET = temp? TEMP_JWT_SECRET : JWT_SECRET
+    return jwt.sign(jwt_data, MY_JWT_SECRET, { expiresIn : expiry})
 }
