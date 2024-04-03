83bf2b4883425e856b95a9452fd87ee32c1ef9ac
doctor-acid <avnishbadoni@gmail.com>
2024-04-03 16:22:13 +0530

    combined routes and cache module ext

---
+++ src/server/dev/devFunctions.ts
@@ -0,0 +1,37 @@
+import * as faker from '@faker-js/faker'
+import { db } from '../db';
+
+export const getDataAndMorph = async function(){
+    let data = faker.en.commerce?.product_name
+    // let mySet: Set<string> = new Set();
+
+    // while(mySet.size<=100){
+    //     let categ = data!.material[Math.floor(Math.random()*data!.material.length)]
+    //                 + ' ' 
+    //                 + data!.product[Math.floor(Math.random()*data!.product.length)];
+    //     mySet.add(categ);
+    // }
+
+    // let myArr:string[] = Array.from(mySet);
+    // type catEntryType = {
+    //     name: string,
+    //     id?: number
+    // }
+    // let myDataArr:catEntryType[] = []
+    // for(let i=0; i<100; i++){
+    //     myDataArr.push({
+    //         name: myArr[i]!,
+    //         id: i+1
+    //     })
+    // }
+
+    // // const dbData = await db.categories.createMany({
+    // //     data: myDataArr
+    // // })
+    // const dbData = await db.categories.findMany()
+    // console.log(dbData)
+
+
+    // await db.categories.deleteMany()
+    // console.log(myDataArr)
+}
